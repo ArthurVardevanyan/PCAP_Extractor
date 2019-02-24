889ea0da0ba049897e859c5d55fb77e61fff5d3c
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2019-02-23 20:09:52 -0500

    Converted File to Variable

--- TShark_data.py
+++ TShark_data.py
@@ -1,19 +1,23 @@
 import os
 
 # Grab the Storage Path, and the FileList from the main program
+
+
 def TShark_data(Spath, fileList):
-    #Strips the PCAP File into usefull information and stores it a temp txt file.
-    os.system("tshark -r "+Spath+"TShark/" + fileList[0]+" -n  -z conv,ip -z endpoints,ip -T fields -e ipv6  >"+Spath+"Tables.txt") 
+    data = []
+    row = 0
+    # Strips the PCAP File into usefull information and stores it a temp txt file.
+    os.system("tshark -r "+Spath+"TShark/" +
+              fileList[0]+" -n  -z conv,ip -z endpoints,ip -T fields -e ipv6  >"+Spath+"Tables.txt")
     count = 0
-    #Makes the data cleaner for scrubbing.
-    with open(Spath+'Tables.txt') as infile, open(Spath+'Table_Condensend.txt', 'w') as outfile:
+    # Makes the data cleaner for scrubbing.
+    with open(Spath+'Tables.txt') as infile:
         for line in infile:
             if not line.strip():
                 continue  # skip the empty line
             if line[0] == "=":
                 count += 1
             if count == 3:
-                outfile.write("\n")
                 count = 0
             if str(line[0]) not in "0123456789":
                 continue
@@ -21,12 +25,13 @@ def TShark_data(Spath, fileList):
             newstr1 = newstr.replace("<", "")
             newstr2 = newstr1.replace("-", "")
             newstr3 = newstr2.replace(">", "")
-            thinned = " ".join(newstr3.split()) + "\n"
+            thinned = " ".join(newstr3.split())
 
-            outfile.write(thinned)  # non-empty line. Write it to output
-    #Puts the Data into a variable
-    #Todo: Remove Unnessary File and Write dircetly to variable.
-    with open(Spath+"Table_Condensend.txt") as textFile:
-        data = [line.split() for line in textFile]
-
+            data.append(thinned.split())
+           
+    # Puts the Data into a variable
+    for line in data:
+        if len(line) == 7:
+            row += 1
+    data.insert(row, [])
     return data
